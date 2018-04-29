5aca33c3cafc6d5719ff6facf8ddf6eb64935cc7
atiqahhhamzah <atiqahamzah@gmail.com>
2018-04-29 01:10:15 -0500

    Block network stuff

--- block_network.py
+++ block_network.py
@@ -1,12 +1,13 @@
 from flask import Flask as flask
 from flask import request
+import requests
 from block import Block
 from block import Blockchain
 node = flask(__name__)
 
 node_transactions = []
-miner_list = []
-blockchain = []
+blockchain = Blockchain()
+peer_list = []
 miner_address = getMiningAddress()
 
 def getMiningAddress():
@@ -31,9 +32,57 @@ def proofOfWork(prev_proof):
 		i+= 1
 	return i
 
+@node.route('/blockchain', methods = ['GET'])
+def getOtherBlocks():
+	updatedBlockChain = consensus()
+	block_dictlist = []
+	for b in updatedBlockChain:
+		block_dictlist.append(
+		{
+			"index": b.index,
+			"timestamp": b.timestamp,
+			"data": b.data,
+			"prev_hash": b.prev_hash,
+			"hash": b.hash
+		})
+	return json.dumps(blockchain_dictlist)
+
+
+def consensus():
+	global blockchain
+	cur_longest_chain = blockchain.getBlockChain()
+
+	peer_chains = []
+	for peer in peer_list():
+		req_response = requests.get("https://%s/blockchain" % peer)
+		if (req_response.status_code == requests.codes.ok):
+			print "Blocks from peer {}: {}\n".format(peer,req_response.content())
+			peer_chains.append(json.loads(req_response.content()))
+
+	for chain in peer_chains:
+		if len(peer_chain) > cur_longest_chain
+			cur_longest_chain = peer_chain
+
+	if(cur_longest_chain != blockchain.getBlockChain()):
+		new_blockchain = []
+		for block in cur_longest_chain:
+			new_blockchain.append(Block(block['index'],block['timestamp'],block['prev_hash'],block['hash']))
+
+		if blockchain.validateChain(new_blockchain) == True:
+			blockchain.updateBlockChain(new_blockchain)
+
+	return blockchain.getBlockChain()
+
+
+
 @node.route('/add_peer',methods = ['GET'])
-	host = request.args
-	
+def addPeer():
+	host = request.get_json()['host']
+	port = request.get_json()['port']
+	peer_list.append(str(host + ':' + port))
+	print "Added {} as peer".format(host)
+	return()
+
 
 @node.route('/mine',methods = ['GET'])
 def mine():
@@ -50,15 +99,17 @@ def mine():
 		"index": new_block_index,
 		"timestamp": mined_block.timestamp,
 		"data": new_block_data,
+		"prev_hash" : mined_block.prev_hash
 		"hash": mined_block.hash
 		}) + '\n'
 	
 
 
 def main(args):
-	blockchain = Blockchain()
-	print "Genesis block #{} initialized".format(blockchain[0].index)
-	print "Hash: {}\n".format(blockchain[0].hash)
+
+	blockchain.populateBlockChain()
+	print "Genesis block #{} initialized".format(blockchain.getCurrBlock().index)
+	print "Hash: {}\n".format(blockchain.getCurrBlock().hash)
 	node = flask(__name__)
 	node.run
 
